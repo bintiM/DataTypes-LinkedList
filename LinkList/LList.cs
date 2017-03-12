98f8ad9a80cb7fbf4b88bae856cd98078dec6fc4
bintiM <marc.bintinger@yahoo.com>
2017-03-12 11:17:26 +0100

    initial

---
+++ LinkList/LList.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkList
+{
+    class LList
+    {
+        Node _firstNode;
+
+        public Node getFirst()
+        {
+            return _firstNode;
+        }
+
+        public Node insertAfter(Node node, Node newNode)
+        {
+            newNode.next = node.next; // setze beim Neuen den Nächsten auf das was vorher der Nächste war
+            node.next = newNode; // setze beim bisherigen Node den next auf den neu eingefügten
+            return newNode;
+        }
+        public Node setFirst(Node first)
+        {
+            _firstNode = first;
+            return _firstNode;
+        }
+
+        public void insertBeginning(Node newNode)
+        {
+            newNode.next = _firstNode.next;
+            _firstNode = newNode;
+        }
+
+        public void insertEnd(Node newNode)
+        {
+            Node aktNode = _firstNode;
+
+            while (aktNode.next != null)
+                aktNode = aktNode.next;
+
+            aktNode.next = newNode;
+        }
+
+        public Node getNodeWithData(int i)
+        {
+            Node aktNode = _firstNode;
+
+            while (aktNode.next != null)
+            {
+                if (aktNode.data == i)
+                    return aktNode;
+                aktNode = aktNode.next;
+            }
+            return null;
+        }
+
+    }
+
+    class Node
+    {
+        private int _data;
+        public int data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+        private Node _next;
+        public Node next
+        {
+            get { return _next; }
+            set { _next = value; }
+        }
+
+
+
+        public Node(int i)
+        {
+            _data = i;
+        }
+    }
+}
---
+++ LinkList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LList l1 = new LList();
+            Node first = l1.setFirst(new Node(7));
+            Node second = l1.insertAfter(first, new Node(12));
+            Node third = l1.insertAfter(second, new Node(28));
+
+
+            LList l2 = new LList();
+            Node node = l2.setFirst(new Node(7));
+            for (int i = 0; i < 20; i++)
+            {
+                node = l2.insertAfter(node, new Node(node.data + 2));
+            }
+
+            node = l2.getFirst();
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(node.data);
+                node = node.next;
+            }
+
+            Node foundNode = l2.getNodeWithData(17);
+
+            Console.WriteLine("gefunden: " + foundNode.data + " und nächster: " + foundNode.next.data);
+
+            Console.ReadKey();
+
+        }
+    }
+    
+}
